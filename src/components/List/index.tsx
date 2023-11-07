5866a3b9feb0068a4557b6428571c3547269f5e1
Dan Shaw <dls1992@live.co.uk>
2023-11-07 16:19:20 +0000

    get, filter, sort and slice list

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,6 +2,19 @@ import { useQuery } from '@tanstack/react-query'
 import getAllProducts from '../../queries/products.queries'
 
 export default function List() {
-    const { data } = useQuery({ queryKey: ['products'], queryFn: getAllProducts })
-    return <pre>{JSON.stringify(data, null, 2)}</pre>
+
+    // grab products
+    const { data: rawResponse } = useQuery({ queryKey: ['products'], queryFn: getAllProducts })
+
+    // filter out brand = Apple
+    const filteredProducts = rawResponse?.products?.filter(product => product.brand !== "Apple")
+
+    // sort highest rating desc
+
+    const sortedProducts = filteredProducts?.sort((a, b) => { return (b?.rating ?? 0) - (a?.rating ?? 0) })
+
+    // choose first 10
+    const displayedProducts = sortedProducts?.slice(0, 9);
+
+    return <pre>{JSON.stringify(displayedProducts, null, 2)}</pre>
 }
--- src/types/product.types.ts
+++ src/types/product.types.ts
@@ -5,16 +5,18 @@ export interface ProductResponse {
   limit: number
 }
 
+// everything marked as optional as API stability is unknown
+// TS will now warn when trying to use a property that may not exist
 export interface Product {
-  'id': number
-  'title': string
-  'description': string
-  'price': number
-  'discountPercentage': number
-  'rating': number
-  'stock': number
-  'brand': string
-  'category': string
-  'thumbnail': string
-  'images': string[]
+  'id'?: number
+  'title'?: string
+  'description'?: string
+  'price'?: number
+  'discountPercentage'?: number
+  'rating'?: number
+  'stock'?: number
+  'brand'?: string
+  'category'?: string
+  'thumbnail'?: string
+  'images'?: string[]
 }
