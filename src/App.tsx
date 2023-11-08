eb68f677172187b2b240cf52bfa07127d442beff
Dan Shaw <dls1992@live.co.uk>
2023-11-08 19:51:35 +0000

    added centered list to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import List from "./components/List";
 import { type ReactNode } from "react";
 
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  max-width: 1500px;
+  margin: 0 auto;
+`;
+
 export default function App(): ReactNode {
-  return <List />;
+  return (
+    <Wrap>
+      <List />
+    </Wrap>
+  );
 }
