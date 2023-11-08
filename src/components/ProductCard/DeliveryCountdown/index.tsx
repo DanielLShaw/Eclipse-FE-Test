0996d160f8487dc18325a021270584f4a8c34a5f
Dan Shaw <dls1992@live.co.uk>
2023-11-08 15:36:32 +0000

    added delivery message
    (without countdown)

--- src/components/ProductCard/DeliveryCountdown/index.tsx
+++ src/components/ProductCard/DeliveryCountdown/index.tsx
@@ -1,3 +1,26 @@
+import { format, startOfTomorrow } from "date-fns";
+import TruckIcon from "../../../assets/icons/truck";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  font-size: 14px;
+`;
+
+const CountdownMessage = styled.span``;
+
 export default function DeliveryCountdown() {
-  return <span>Deliver Countdown</span>;
+  const deliveryDay = format(startOfTomorrow(), "do MMMM");
+  return (
+    <Wrap>
+      <TruckIcon />
+      <CountdownMessage>
+        Order in the next <strong>XX:XX:XX</strong> for delivery on{" "}
+        <strong>{deliveryDay}</strong>
+      </CountdownMessage>
+    </Wrap>
+  );
 }
