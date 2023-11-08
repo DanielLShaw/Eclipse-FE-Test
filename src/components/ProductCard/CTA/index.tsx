ef9f8e34021e715faedbee0fec4a59a88afcddbd
Dan Shaw <dls1992@live.co.uk>
2023-11-08 15:18:55 +0000

    added CTA styles

--- src/components/ProductCard/CTA/index.tsx
+++ src/components/ProductCard/CTA/index.tsx
@@ -1,8 +1,50 @@
+import styled from "styled-components";
+import CartIcon from "../../../assets/icons/cart";
+
 interface CTAProps {
   text: string;
   onClick: () => void;
 }
 
+const Button = styled.button`
+  appearance: none;
+  border: none;
+
+  border-radius: 25px;
+  background: var(--Pink-Gradient);
+  color: white;
+
+  font-size: 16px;
+  font-weight: 600;
+  font-family: inherit;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+
+  height: 42px;
+  padding: 0 20px;
+  width: 100%;
+
+  cursor: pointer;
+
+  &:hover {
+    background-size: 150% 150%;
+  }
+`;
+
+const StyledCartIcon = styled(CartIcon)`
+  fill: white;
+  height: 18px;
+  width: 18px;
+`;
+
 export default function CTA({ text, onClick }: CTAProps) {
-  return <button onClick={onClick}>{text}</button>;
+  return (
+    <Button onClick={onClick}>
+      <StyledCartIcon />
+      {text}
+    </Button>
+  );
 }
